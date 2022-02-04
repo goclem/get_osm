9b65cc7ea2cc2fe91990d2d53de178a00a425249
goclem <clement.gorin@outlook.com>
2022-02-04 10:41:43 +0100

    Added a "waiting for server" feature which avoids errors when there is no available slot.

--- get_osm.R
+++ get_osm.R
@@ -2,23 +2,35 @@
 # Description: Extracts OSM data using the Overpass API
 # Author: Clement Gorin
 # Contact: gorinclem@gmail.com
-# Date: February 2021
+# Version: 2022.02.04
 
 # Packages
 if(!require("pacman")) install.packages("pacman", repos = "https://cloud.r-project.org/")
-pacman::p_load(curl, geojsonsf, RCurl, sf, tictoc, tmap)
+pacman::p_load(curl, geojsonsf, RCurl, sf, stringr, tictoc, tmap)
 
 # Functions ---------------------------------------------------------------
 
 # Checks requirements
 check_requirements <- function(server = "http://overpass-api.de/api/status") {
   cat("osmtogeojson", system("osmtogeojson --version"), "\n\n")
-  cat(RCurl::getURL(server))    
+  cat(RCurl::getURL(server))
+}
+
+# Checks server availability
+check_server <- function(server = "http://overpass-api.de/api/status") {
+  status <- RCurl::getURL(server)
+  slots  <- stringr::str_extract(status, "\\d(?= slots available now)")
+  if(is.na(slots)) {
+    wait <- stringr::str_extract(status, "\\d+(?= seconds)")
+    cat(sprintf("Server: Waiting %s seconds...\n", wait))
+    Sys.sleep(as.numeric(wait))
+  }
 }
 
 # Extracts OSM data
 get_osm <- function(query, server = "http://www.overpass-api.de/api/interpreter") {
-  cat("Query:\n", query, "\n")
+  cat(sprintf("Query:\n%s\n", str_replace_all(query, ";", "\n")))
+  check_server()
   tictoc::tic("Runtime")  
   query <- paste0(server, "?data=", URLencode(query))
   files <- sapply(c("osm", "geojson"), function(ext) tempfile(fileext = paste0(".", ext)))
@@ -27,7 +39,7 @@ get_osm <- function(query, server = "http://www.overpass-api.de/api/interpreter"
   response <- geojsonsf::geojson_sf(files["geojson"])
   response <- sf::st_make_valid(response)
   unlink(files, recursive = T)
-  cat("Features:", nrow(response), "\n")
+  cat(sprintf("Features: %d\n", nrow(response)))
   tictoc::toc()
   return(response)
 }
